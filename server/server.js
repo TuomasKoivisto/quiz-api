288390dd8dfaa1baedbe19bbb20f5e548066c286
Tuomas Koivisto <koivisto.tuomas@gmail.com>
2018-03-10 19:08:04 +0200

    setup for heroku

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var { mongoose } = require('./db/mongoose');
 var { Question } = require('./models/question');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -37,8 +38,8 @@ app.get('/questions', (req, res) => {
   );
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started up at ${port}`);
 });
 
 module.exports = { app };
